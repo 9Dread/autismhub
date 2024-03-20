72d5d1fd8b3ecd93a74860bed1e7cac260f1e095
9Dread <marcojihv@gmail.com>
2024-03-19 21:41:16 -0400

    upd

--- housepricepred.R
+++ housepricepred.R
@@ -4,5 +4,9 @@ train <- read_csv("train.csv")
 nacount <- train |> 
   summarize(across(everything(), ~ sum(is.na(.))))
 
+numtrain <- train |> 
+  select(where(is.numeric)) |> 
+  select()
+
 ggplot(train, mapping = aes(x = MSZoning)) +
   geom_bar()
