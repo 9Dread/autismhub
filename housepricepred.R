d60280fc61b1976dee8f6309b1f6b6fb430f6ba3
9Dread <marcojihv@gmail.com>
2024-03-25 22:36:13 -0400

    rf model

--- housepricepred.R
+++ housepricepred.R
@@ -1,20 +1,31 @@
 library(tidyverse)
 library(caret)
 library(rpart.plot)
+library(reshape2) #cor mat
+library(ggthemes)
+library(randomForest)
+library(janitor)
 train <- read_csv("train.csv")
 test <- read_csv("test.csv")
-numtraintest <- test |> 
-  select(MSSubClass, OverallQual, OverallCond)
+numtest <- test |> 
+  select(!c(LotFrontage, MasVnrArea, GarageYrBlt, SalePrice))
+numtest <- clean_names(numtrain)
+
+numtest2 <- test |> 
+  select(c(MSSubClass, OverallQual, OverallCond))
+numtrain2 <- train |> 
+  select(c(MSSubClass, OverallQual, OverallCond, SalePrice))
 
 nacount <- train |> 
   summarize(across(everything(), ~ sum(is.na(.))))
 
 numtrain <- train |> 
   select(where(is.numeric)) |> 
-  select(MSSubClass, OverallQual, OverallCond, SalePrice)
+  select(!c(LotFrontage, MasVnrArea, GarageYrBlt))
 
 nacount <- numtrain |> 
   summarize(across(everything(), ~ sum(is.na(.))))
+
 
 tc <- trainControl(method = "repeatedcv", number = 10, repeats = 3, savePredictions = "all", returnResamp = "all")
 set.seed(3333)
@@ -23,7 +34,7 @@ set.seed(3333)
 #https://tungmphung.com/information-gain-gain-ratio-and-gini-index/ for difference between those things
 #
 #What does tunelength do?
-dtree_fit <- train(SalePrice ~ ., data = numtrain, method = "rpart", 
+dtree_fit <- train(SalePrice ~ ., data = numtrain2, method = "rpart", 
                    trControl = tc, parms = list(split = "information"), tuneLength = 10)
 # dtree_fit$results can see what is saved by savepreds and returnresamp in tc
 
@@ -34,5 +45,20 @@ prp(dtree_fit$finalModel, box.palette = "Reds", tweak = 1.2)
 
 submission <- tibble(Id = 1461:2919, SalePrice = predict(dtree_fit, newdata = numtraintest))
 write_csv(submission, "submission.csv")
-ggplot(train, mapping = aes(x = MSZoning)) +
-  geom_bar()
+
+cormat <- round(cor(numtrain), 2)
+meltedCormat <- melt(cormat)
+ggplot(meltedCormat, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile() +
+  scale_color_binned() +
+  theme_calc()
+
+numtrain <- clean_names(numtrain)
+rfMod <- randomForest(formula = sale_price ~., data = numtrain)
+which.min(rfMod$mse)
+sqrt(rfMod$mse[which.min(rfMod$mse)])
+plot(rfMod)
+varImpPlot(rfMod)
+
+submission <- tibble(Id = 1461:2919, SalePrice = predict(rfMod, newdata = numtest))
+write_csv(submission, "submission.csv")
